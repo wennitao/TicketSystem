78e2576b753a9007ee01bbc5e7b2456da1cc3618
AegeanYan <1203616626@qq.com>
2021-05-10 14:07:53 +0800

    new file

---
+++ Bpt_and_database.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 罗皓天 on 2021/5/10.
+//
+
+#include "Bpt_and_database.h"
---
+++ Bpt_and_database.h
@@ -0,0 +1,14 @@
+//
+// Created by 罗皓天 on 2021/5/10.
+//
+
+#ifndef TICKETSYSTEM_BPT_AND_DATABASE_H
+#define TICKETSYSTEM_BPT_AND_DATABASE_H
+
+
+class Bpt_and_database {
+
+};
+
+
+#endif //TICKETSYSTEM_BPT_AND_DATABASE_H
