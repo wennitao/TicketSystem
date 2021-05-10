852e780298d14f1ef21a23a6b3862dbec05c1f1a
wennitao <wennitao@126.com>
2021-05-10 14:07:14 +0800

    update user.hpp

---
+++ user.hpp
@@ -0,0 +1,37 @@
+#ifndef TicketSystem_user
+#define TicketSystem_user
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+class user {
+
+    friend ostream& operator << (ostream &out, const user &_user) ;
+
+private:
+    char username[25], password[35], name[30], mailAddr[35] ;
+    int privilege ;
+
+public:
+    user () {}
+
+    user (const char *_username, const char *_password, const char *_name, const char *_mailAddr, int p) {
+        strcpy (username, _username) ;
+        strcpy (password, _password) ;
+        strcpy (name, _name) ;
+        strcpy (mailAddr, _mailAddr) ;
+        privilege = p ;
+    }
+
+
+} ;
+
+ostream& operator << (ostream &out, const user &_user) {
+    out << "username:" << _user.username << " password:" << _user.password << " name:" << _user.name << " mailAddress:" << _user.mailAddr << " privilege:" << _user.privilege ;
+    return out ;
+}
+
+#endif
