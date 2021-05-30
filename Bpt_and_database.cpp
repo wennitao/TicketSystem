3a204056cb21b1afac7f1c321e28ab610c24842a
AegeanYan <1203616626@qq.com>
2021-05-30 15:13:05 +0800

    修改了Bpt_and_database.h及.cpp 修改了data.h 加入了Bptest.cpp 修改了Cmakelist

--- Bpt_and_database.cpp
+++ Bpt_and_database.cpp
@@ -28,6 +28,8 @@ void Database::node::print() {
     for (int i = 0; i <= size; ++i) {
         cout << son[i] << ' ' ;
     }
+    cout << endl;
+    cout << "lbro: " << lbro << " rbro: " << rbro ;
     cout << endl << "key: " << endl;
     for (int i = 0; i < keycnt; ++i) {
         cout << key[i].str << ' ' << key[i].pos << endl;
@@ -41,7 +43,7 @@ void Database::print(int nod) {
     cout << "pos: " << nod << endl;
     cur.print();
     cout << endl;
-    for (int i = 0; i < cur.keycnt ; ++i) {// < 还是 <=
+    for (int i = 0; i <= cur.keycnt ; ++i) {//是 <=
         if (cur.son[i] != -1){
             print(cur.son[i]);
         }
@@ -59,6 +61,7 @@ void Database::insert(const data &x) {
         node cur = disk_read(tmp_pos);
 //        cur.key[cur.keycnt ++] = x;
         insert_sort(cur.key , cur.keycnt , x);
+        cur.keycnt++;
         if (cur.keycnt <= maxkey) disk_write(tmp_pos , cur);//先插再分裂
         else {
             node nxt;
@@ -122,7 +125,7 @@ void Database::insert(int &pa, int lchild, int rchild, const data &x) {
             }
             nxt_node.son[pa_node.keycnt - (maxkey / 2 + 1)] = pa_node.son[pa_node.keycnt];
             update_son_fa(pa_node.son[pa_node.keycnt] , nxt_pos);
-
+            pa_node.son[pa_node.keycnt] = -1;
             pa_node.keycnt = maxkey / 2;
             data tmp = pa_node.key[maxkey / 2];
             clear(pa_node.key[maxkey / 2]);
@@ -152,31 +155,64 @@ void Database::find(const data &x, vector<int> &cap) {
     find (root , x , cap);
 };
 void Database::find(int nod, const data &x, vector<int> &cap) {
-    if (nod == -1) return;
+//    if (nod == -1) return;
+//    node cur = disk_read(nod);
+//    int pos = 0;
+//    for (;pos < cur.keycnt && cur.key[pos] < x ; pos ++);
+//    if (cur.is_leaf){
+//        for (int i = pos ; i < cur.keycnt; ++i) {
+//            if (strcmp(cur.key[i].str , x.str) == 0)cap.push_back(cur.key[i].pos);
+//            else return;
+//        }
+//    } else{
+//        if (pos == cur.keycnt || strcmp(x.str , cur.key[pos].str) < 0)find(cur.son[pos] , x , cap);
+//        else {
+//            vector<int> tmp;
+//            find (cur.son[pos] , x ,tmp);
+//            for (int i : tmp) {
+//                cap.push_back(i);
+//            }
+//            for(; pos < cur.keycnt && strcmp(cur.key[pos].str , x.str) == 0 ; ++pos){
+//                tmp.clear();
+//                find(cur.son[pos + 1] , x , tmp);
+//                for (int i : tmp) {
+//                    cap.push_back(i);
+//                }
+//            }
+//        }
+//    }
+    if (nod == -1)return;
     node cur = disk_read(nod);
+
+    while (!cur.is_leaf){
+        int pos = 0;
+        for (;pos < cur.keycnt && cur.key[pos] < x ; pos++);
+        cur = disk_read(cur.son[pos]);
+    }
     int pos = 0;
-    for (;pos < cur.keycnt && cur.key[pos] < x ; pos ++);
-    if (cur.is_leaf){
-        for (int i = pos ; i < cur.keycnt; ++i) {
-            if (strcmp(cur.key[i].str , x.str) == 0)cap.push_back(cur.key[i].pos);
+    while (strcmp(cur.key[pos].str , x.str) < 0){
+        if (pos == cur.keycnt - 1){
+            if (cur.rbro != -1){cur = disk_read(cur.rbro);pos = 0;}
             else return;
         }
-    } else{
-        if (pos == cur.keycnt || strcmp(x.str , cur.key[pos].str) < 0)find(cur.son[pos] , x , cap);
-        else {
-            vector<int> tmp;
-            find (cur.son[pos] , x ,tmp);
-            for (int i : tmp) {
-                cap.push_back(i);
-            }
-            for(; pos < cur.keycnt && strcmp(cur.key[pos].str , x.str) == 0 ; ++pos){
-                tmp.clear();
-                find(cur.son[pos + 1] , x , tmp);
-                for (int i : tmp) {
-                    cap.push_back(i);
-                }
-            }
-        }
+        else pos++;
+    }
+//    while (strcmp(x.str , cur.key[pos].str) < 0){
+//        if (pos == cur.keycnt - 1){
+//            if (cur.rbro != -1)cur = disk_read(cur.rbro);
+//            else return;
+//        }
+//        cap.push_back(cur.key[pos].pos);
+//        pos++;
+//    }
+    while (strcmp(cur.key[pos].str , x.str) == 0){
+        cap.push_back(cur.key[pos].pos);
+        if (pos == cur.keycnt - 1){
+            if (cur.rbro != -1){
+                cur = disk_read(cur.rbro);
+                pos = 0;
+            } else return;
+        } else pos ++;
     }
 };
 int Database::search(int nod, const data &x) {//find the leaf_node where can insert x//return the node
@@ -192,8 +228,142 @@ void Database::clear(data &tmp) {
     memset(tmp.str , 0 , sizeof(tmp.str));
     tmp.pos = -1;
 };
-void Database::checkpapa(node &pa) {
+void Database::checkpapa(int pa) {
     //
+    if (pa == -1)return;
+    node cur = disk_read(pa);
+    if (cur.keycnt >= maxkey / 2)return;
+    if (cur.keycnt == 0){
+        root = cur.son[0];
+        node son = disk_read(cur.son[0]);
+        son.fa = -1;
+        update_root();
+    }
+    int gpar = cur.fa;
+    if (gpar == -1)return;
+    node gpar_node = disk_read(gpar);
+    int pa_pos = 0;
+    for (;gpar_node.son[pa_pos] != pa ; ++pa_pos);
+    int luncle = -1;
+    int runcle = -1;
+    node lun_node;
+    node run_node;
+    if (pa_pos != 0){
+        luncle = gpar_node.son[pa_pos - 1];
+        lun_node = disk_read(luncle);
+    }
+    if (pa_pos != gpar_node.keycnt){
+        runcle = gpar_node.son[pa_pos + 1];
+        run_node = disk_read(runcle);
+    }//先尝试去借
+    if (luncle != -1 && lun_node.keycnt > maxkey/2){
+        for (int i = cur.keycnt; i > 0 ; --i) {
+            cur.key[i] = cur.key[i - 1];
+        }
+        cur.key[0] = gpar_node.key[pa_pos - 1];
+        cur.keycnt++;
+        for (int i = cur.keycnt; i > 0 ; --i) {
+            cur.son[i] = cur.son[i - 1];
+        }
+        cur.son[0] = run_node.son[run_node.keycnt];
+        node son_change = disk_read(cur.son[0]);
+        son_change.fa = pa;
+        disk_write(cur.son[0] , son_change);
+        gpar_node.key[pa_pos - 1] = run_node.key[run_node.keycnt - 1];
+        clear(run_node.key[run_node.keycnt - 1]);
+        run_node.keycnt--;
+        disk_write(pa , cur);
+        disk_write(gpar , gpar_node);
+        disk_write(luncle , lun_node);
+    } else if (runcle != -1 && run_node.keycnt > maxkey/2){
+        cur.key[cur.keycnt++] = gpar_node.key[pa_pos];
+        cur.son[cur.keycnt] = run_node.son[0];
+        node son_change = disk_read(run_node.son[0]);
+        son_change.fa = pa;
+        disk_write(run_node.son[0] , son_change);
+        gpar_node.key[pa_pos] = run_node.key[0];
+        for (int i = 0; i < run_node.keycnt - 1; ++i) {
+            run_node.key[i] = run_node.key[i + 1];
+        }
+        clear(run_node.key[run_node.keycnt - 1]);
+        run_node.keycnt--;
+        for (int i = 0; i <= run_node.keycnt; ++i) {
+            run_node.son[i] = run_node.son[i + 1];
+        }
+        run_node.son[run_node.keycnt + 1] = -1;
+        disk_write(pa , cur);
+        disk_write(gpar , gpar_node);
+        disk_write(runcle , run_node);
+    } else if (luncle != -1){//突然想起来erase合并里面的lbro和rbro还没改//改完了
+        lun_node.key[lun_node.keycnt] = gpar_node.key[pa_pos - 1];
+        for (int i = 0; i < cur.keycnt; ++i) {
+            lun_node.key[lun_node.keycnt + 1 + i] = cur.key[i];
+            lun_node.son[lun_node.keycnt + 1 + i] = cur.son[i];
+        }
+        lun_node.son[lun_node.keycnt + cur.keycnt + 1] = cur.son[cur.keycnt];
+        lun_node.keycnt += cur.keycnt + 1;
+        for (int i = 0; i <= cur.keycnt; ++i) {
+//            node sonson = disk_read(cur.son[i]);
+//            sonson.fa = luncle;
+//            disk_write(cur.son[i] , sonson);
+            update_son_fa(cur.son[i] , luncle);
+        }
+        //clear(cur.key)不想写，省了，应该没有问题
+        for (int i = pa_pos - 1; i < gpar_node.keycnt - 1; ++i) {
+            gpar_node.key[i] = gpar_node.key[i + 1];
+        }
+        clear(gpar_node.key[gpar_node.keycnt - 1]);
+        for (int i = pa_pos; i < gpar_node.keycnt; ++i) {
+            gpar_node.son[i] = gpar_node.son[i + 1];
+        }
+        gpar_node.son[gpar_node.keycnt] = -1;
+        gpar_node.keycnt--;
+        //cur被删了，此处可加缓存
+        if (gpar == root && gpar_node.keycnt == 0){
+            root = luncle;
+            lun_node.fa = -1;
+            update_root();
+            //此处gpar也被删了可加缓存
+        }
+        disk_write(pa , cur);
+        disk_write(luncle , lun_node);
+        disk_write(gpar , gpar_node);
+        checkpapa(lun_node.fa);
+    } else {
+        cur.key[cur.keycnt] = gpar_node.key[pa_pos];
+        for (int i = 0; i < run_node.keycnt; ++i) {
+            cur.key[cur.keycnt + 1 + i] = run_node.key[i];
+            cur.son[cur.keycnt + 1 + i] = run_node.son[i];
+        }
+        cur.son[cur.keycnt + run_node.keycnt + 1] = run_node.son[run_node.keycnt];
+        cur.keycnt += run_node.keycnt + 1;
+        for (int i = 0; i <= run_node.keycnt; ++i) {
+//            node sonson = disk_read(run_node.son[i]);
+//            sonson.fa = pa;
+//            disk_write(run_node.son[i] , sonson);
+            update_son_fa(run_node.son[i] , pa);
+        }
+        //删除了run_node可加缓存
+        for (int i = pa_pos; i < gpar_node.keycnt - 1; ++i) {
+            gpar_node.key[i] = gpar_node.key[i + 1];
+        }
+        clear(gpar_node.key[gpar_node.keycnt - 1]);
+        for (int i = pa_pos + 1; i < gpar_node.keycnt ; ++i) {
+            gpar_node.son[i] = gpar_node.son[i + 1];
+        }
+        gpar_node.son[gpar_node.keycnt] = -1;
+        gpar_node.keycnt--;
+        if(gpar == root && gpar_node.keycnt == 0){
+            root = pa;
+            cur.fa = -1;
+            update_root();
+            //此处也可加gpar的缓存
+        }
+        disk_write(pa , cur);
+        disk_write(runcle , run_node);
+        disk_write(gpar , gpar_node);
+        checkpapa(cur.fa);
+    }
 }
 void Database::erase(const data &x) {
     pair<int , int> pos = find(x);
@@ -205,6 +375,7 @@ void Database::erase(const data &x) {
     clear(cur.key[cur.keycnt - 1]);
     cur.keycnt--;
     if (pos.first == root || cur.keycnt >= maxkey/2){
+
         disk_write(pos.first , cur);
     } else{
         if (cur.fa == -1){
@@ -236,14 +407,14 @@ void Database::erase(const data &x) {
             cur.key[cur.keycnt ++] = rbro_node.key[0];
             for (int i = 1; i < rbro_node.keycnt; ++i) {
                 rbro_node.key[i - 1] = rbro_node.key[i];
-                clear(rbro_node.key[rbro_node.keycnt - 1]);
-                rbro_node.keycnt--;
-                par_node.key[son_pos] = rbro_node.key[0];
-
-                disk_write(pos.first , cur);
-                disk_write(rbro , rbro_node);
-                disk_write(cur.fa , par_node);
             }
+            clear(rbro_node.key[rbro_node.keycnt - 1]);
+            rbro_node.keycnt--;
+            par_node.key[son_pos] = rbro_node.key[0];
+
+            disk_write(pos.first , cur);
+            disk_write(rbro , rbro_node);
+            disk_write(cur.fa , par_node);
         } else{
             //合并删除
             //我会尽量去找同父亲的兄弟合并
@@ -260,14 +431,19 @@ void Database::erase(const data &x) {
                     par_node.key[i - 1] = par_node.key[i];
                     //par_node.son[i] = par_node.son[i + 1];
                 }
-                for (int i = son_pos + 1; i < par_node.keycnt; ++i) {
+                for (int i = son_pos + 1; i <= par_node.keycnt; ++i) {
                     par_node.son[i - 1] = par_node.son[i];
                 }
-                par_node.son[par_node.keycnt - 1] = -1;
+                par_node.son[par_node.keycnt] = -1;
                 clear(par_node.key[par_node.keycnt - 1]);
                 par_node.keycnt--;
                 for (int i = 0; i < cur.keycnt; ++i) {
                     lbro_node.key[lbro_node.keycnt + i] = cur.key[i];
+                }//cur是被删掉的
+                lbro_node.rbro = rbro;
+                if (rbro != -1){
+                    rbro_node.lbro = lbro;
+                    disk_write(rbro , rbro_node);
                 }
                 lbro_node.keycnt += cur.keycnt;
                 cur.keycnt = 0;
@@ -279,14 +455,20 @@ void Database::erase(const data &x) {
                 for (int i = son_pos + 1 ; i < par_node.keycnt; ++i) {
                     par_node.key[i - 1] = par_node.key[i];
                 }
-                for (int i = son_pos + 2; i < par_node.keycnt; ++i) {
+                for (int i = son_pos + 2; i <= par_node.keycnt; ++i) {
                     par_node.son[i - 1] = par_node.son[i];
                 }
-                par_node.son[par_node.keycnt - 1] = -1;
+                par_node.son[par_node.keycnt] = -1;
                 clear(par_node.key[par_node.keycnt - 1]);
                 par_node.keycnt--;
                 for (int i = 0; i < rbro_node.keycnt; ++i) {
                     cur.key[cur.keycnt + i] = rbro_node.key[i];
+                }//删除rbro
+                cur.rbro = rbro_node.rbro;
+                if (rbro_node.rbro != -1){
+                    node rrbro_node = disk_read(rbro_node.rbro);
+                    rrbro_node.lbro = pos.first;
+                    disk_write(rbro_node.rbro , rrbro_node);
                 }
                 cur.keycnt += rbro_node.keycnt;
                 rbro_node.keycnt = 0;
@@ -296,7 +478,7 @@ void Database::erase(const data &x) {
                 disk_write(rbro , rbro_node);
             }
             //只有合并删除需要去看非叶节点合并和借的情况
-            checkpapa(par_node);
+            checkpapa(cur.fa);
             //
         }
     }
@@ -313,9 +495,7 @@ void Database::disk_write(int pos, node &x) {
     io.seekp(pos + init_offset , ios::beg);
     io.write(reinterpret_cast<char *>(&x) , sizeof(node));
 };
-void Database::erase_par(int nod) {
 
-};
 int Database::findKey(const data &x) {
     pair<int , int> pos = find(x);
     if (pos.first == -1) return -1;
